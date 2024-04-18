81880aebea19137e67f3aad0d3fac9bd6cb47bbc
mazamgo <mauricio.zambrana.g@gmail.com>
2024-04-18 13:55:45 -0600

    Agrego Paquetes

---
+++ ProyectoIdentity/Datos/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoIdentity.Datos
+{
+
+    //IdentityDbContext: contiene una serie de Herramientas, se crean tabla de usuarios, roles, todo lo necesita para la autenticacion
+    //DbContext:
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        //Agregar los modelos necesarios
+
+    }
+}
---
+++ ProyectoIdentity/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProyectoIdentity.Datos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configuracion a SQL SERVER:
+builder.Services.AddDbContext<ApplicationDbContext>(opciones => 
+opciones.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql")));
+
+//Agregar el servicio Identity a la aplicaciom
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication(); //Se agrego la autenticacion.
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
